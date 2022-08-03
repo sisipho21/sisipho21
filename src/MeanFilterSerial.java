429e4374a4f25c9d0a57f45540925053f4ae6951
Sisipho <tkssis001@myuct.ac.za>
2022-08-03 17:37:48 +0200

    Reading in an image file

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -1,5 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 public class MeanFilterSerial {
+    
+    
+    
      public static void main(String[] args) {
-        System.out.println("Mean Filter Serial");
+        BufferedImage img = null;
+        File f = null;
+
+        String inImage = args[0];
+        String path = "../images/"+inImage;
+        
+        
+        try {
+ 
+            f = new File(path);
+            img = ImageIO.read(f);
+            
+        } catch (IOException e) {
+            System.out.println("File not found!");
+            System.exit(0);
+        }
+        System.out.println("File successfully found!");
+        
     }
 }
--- src/MedianFilterSerial.java
+++ src/MedianFilterSerial.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 public class MedianFilterSerial {
     public static void main(String[] args) {
-        System.out.println("Median Filter Serial");
+        BufferedImage img = null;
+        File f = null;
+
+        String inImage = args[0];
+        String path = "../images/"+inImage;
+        
+        
+        try {
+ 
+            f = new File(path);
+            img = ImageIO.read(f);
+            
+        } catch (IOException e) {
+            System.out.println("File not found!");
+            System.exit(0);
+        }
+        System.out.println("File successfully found!"); 
     }
 }
