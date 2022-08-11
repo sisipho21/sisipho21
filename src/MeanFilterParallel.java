ab6b4a90f60c1057a64f5efbc6429701e46bab44
Sisipho <tkssis001@myuct.ac.za>
2022-08-11 19:11:29 +0200

    Created median filter parallel

--- src/MeanFilterParallel.java
+++ src/MeanFilterParallel.java
@@ -7,22 +7,24 @@ import javax.imageio.ImageIO;
 
  public class MeanFilterParallel{  //Outer class
     
+    //Variables shared between the outer class and the inner class
+    static BufferedImage img = null, copy=null;
+    static int width, window;
     public static void main(String[] args) {  //main method for outer class
-        BufferedImage img = null, copy=null;
         File f = null, f2=null;
-        int width, height;
+        int height;
 
         String inImage = args[0];  //input image
         String path1 = "../images/"+inImage;
         String outImage = args[1]; //output image
         String path2 = "../images/"+outImage;
-        int window_w = Integer.valueOf(args[2]); //window width
+        window = Integer.valueOf(args[2]); //window width
         
         //Ensuring that window index>=3 and odd
-        if (window_w < 3) {
+        if (window < 3) {
             System.out.println("Please provide index >=3");
             System.exit(0);
-        } else if (window_w %2 == 0) {
+        } else if (window %2 == 0) {
             System.out.println("Please provide an odd index");
             System.exit(0);
         }
@@ -43,31 +45,8 @@ import javax.imageio.ImageIO;
         width = img.getWidth();
         height = img.getHeight();
 
-        //Ensuring that small pictures are not filtered
-        /*if (width<3) {
-            System.out.println("Image width is too small to be filtered.");
-            try {
-                f2 = new File(path2) ;
-                ImageIO.write(copy,"jpg", f2);
-             } catch (IOException e) {
-                 System.out.println("Could not write to output image");
-                 System.exit(0);
-             }
-            System.exit(0);
-        } else if (height<3) {
-            System.out.println("Image height is too small to be filtered.");
-            try {
-                f2 = new File(path2) ;
-                ImageIO.write(copy,"jpg", f2);
-             } catch (IOException e) {
-                 System.out.println("Could not write to output image");
-                 System.exit(0);
-             }
-            System.exit(0);
-        }*/
-
         MeanFilterParallel meanF = new MeanFilterParallel();
-        RunMeanFilterParallel mfp = meanF.new RunMeanFilterParallel(img, copy, window_w, 0, height-1, width);
+        RunMeanFilterParallel mfp = meanF.new RunMeanFilterParallel( 0, height);
         ForkJoinPool fjPool = new ForkJoinPool();
 
         fjPool.invoke(mfp);
@@ -86,23 +65,15 @@ import javax.imageio.ImageIO;
 
     //INNER CLASS START
     public class RunMeanFilterParallel extends RecursiveAction{
-    private int SEQUENTIAL_CUTOFF = 150;
-    private int window = 3;
+    private int SEQUENTIAL_CUTOFF = 500;
     private int starting_H;
     private int ending_H;
-    private int w;
-    BufferedImage srcImg;
-    BufferedImage dstImg;
-    int theHeight;
+    private int windHalf = window/2;
 
-    public RunMeanFilterParallel(BufferedImage sourceImage, BufferedImage destImage, int window_width, int start_height, int end_height, int width) {
-        window = window_width;
+    public RunMeanFilterParallel( int start_height, int end_height) {
+        //window = window_width;
         starting_H = start_height;
         ending_H = end_height;
-        w = width;
-        srcImg = sourceImage;
-        dstImg = destImage;
-        theHeight = ending_H - starting_H;
     }
     
     //executes mean filter in serial  **Start of Filtering
@@ -110,7 +81,7 @@ import javax.imageio.ImageIO;
         //long startTime = System.currentTimeMillis(); //start time for benchmarking
         int width_squared = window*window;
         for (int i = starting_H; i < ending_H-window; i++) {
-            for (int j = 0; j < w-window; j++) {
+            for (int j = 0; j < width-window; j++) {
                 int hStart = i; int hEnd = hStart +window-1; int hMiddle = (hStart+hEnd)/2;
                 int wStart = j; int wEnd = wStart +window-1; int wMiddle = (wStart+wEnd)/2;
                 int rSum=0, gSum=0, bSum=0;
@@ -118,7 +89,7 @@ import javax.imageio.ImageIO;
                 for (int y = 0; y < window; y++) {
                     
                     for (int x = 0; x < window; x++) {
-                        int p = srcImg.getRGB(wStart, hStart);
+                        int p = img.getRGB(wStart, hStart);
                         rSum += (p>>16) & 0xff;
                         gSum += (p>>8) & 0xff;
                         bSum += p & 0xff;
@@ -132,18 +103,18 @@ import javax.imageio.ImageIO;
                 int gMean = gSum/width_squared;
                 int bMean = bSum/width_squared;
                 int filtered_pixel =  (rMean<<16) | (gMean<<8) | bMean;
-                dstImg.setRGB(wMiddle, hMiddle, filtered_pixel);
+                copy.setRGB(wMiddle, hMiddle, filtered_pixel);
             }
         }
     }// **End of Mean Filter
 
     protected void compute() {
-        if (theHeight < SEQUENTIAL_CUTOFF) {
+        if (ending_H-starting_H < SEQUENTIAL_CUTOFF) {
             computeMeanFilter();
             return;
         }
-        int middle = ending_H/2;
-        invokeAll(new RunMeanFilterParallel(srcImg, dstImg, window, starting_H, middle, w), new RunMeanFilterParallel(srcImg, dstImg, window, starting_H+middle, middle, w) );
+        int middle = (starting_H+ending_H)/2;
+        invokeAll(new RunMeanFilterParallel(starting_H, middle+windHalf ), new RunMeanFilterParallel( middle-windHalf, ending_H) );
     }
 
     
--- src/MedianFilterParallel.java
+++ src/MedianFilterParallel.java
@@ -1,21 +1,25 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class MedianFilterParallel {
-    public static void main(String[] args) {
-        MedianFilterParallel medP = new MedianFilterParallel();
-        BufferedImage img = null, copy=null;
-        File f = null;
-        int w,h;
+    //global variables for both
+    static BufferedImage img = null, copy=null;
+    static int width, window;
+    //START MAIN
+    public static void main(String[] args){
+        File f = null, f2=null;
+        int height;
 
         String inImage = args[0];  //input image
         String path1 = "../images/"+inImage;
         String outImage = args[1]; //output image
         String path2 = "../images/"+outImage;
-        int window = Integer.valueOf(args[2]); //window width
-
+        window = Integer.valueOf(args[2]); //window width
+        
         //Ensuring that window index>=3 and odd
         if (window < 3) {
             System.out.println("Please provide index >=3");
@@ -24,6 +28,7 @@ public class MedianFilterParallel {
             System.out.println("Please provide an odd index");
             System.exit(0);
         }
+
         
         //Reading in image from images folder
         try {
@@ -37,57 +42,78 @@ public class MedianFilterParallel {
             System.exit(0);
         }
         
-        w = img.getWidth();
-        h = img.getHeight();
+        width = img.getWidth();
+        height = img.getHeight();
 
-        //Ensuring that small pictures are not filtered
-        if (w<3) {
-            System.out.println("Image width is too small to be filtered.");
-            medP.OutputImage(copy, path2);
-            System.exit(0);
-        } else if (h<3) {
-            System.out.println("Image height is too small to be filtered.");
-            medP.OutputImage(copy, path2);
-            System.exit(0);
-        }
+        MedianFilterParallel medianF = new MedianFilterParallel();
+        RunMedianFilterParallel mfp = medianF.new RunMedianFilterParallel( 0, height);
+        ForkJoinPool fjPool = new ForkJoinPool();
 
-        //writing to a file using pixels
-        for (int i=0; i<h; i++)
-        {
-            for (int j = 0; j < w; j++) {
+        fjPool.invoke(mfp);
 
-                int p = img.getRGB(j, i); //pixel from original image
-                int alpha = (p>>24) & 0xff; //bits 24 to 31
-                int red = (p>>16) & 0xff; //bits 16 to 23
-                int green = (p>>8) & 0xff; //bits 8 to 15
-                int blue = p & 0xff; //0 to 7 bits
-                
-                int p2 = (alpha<<24) | (red<<16) | (green<<8) | blue; //pixel for new image
-
-                copy.setRGB(j, i, p2); //create the value at the pixel coordinate
-
-            }
-        }
-
-        //Outputing the image file into another image
-        medP.OutputImage(copy, path2);
-        
-    }
-
-    /**
-     * Creates an image file using the path and image you provide
-     * @param image is a BufferedImage object
-     * @param thePath is a string value with the path of the folder where image will be created
-     * @throws IOException when the image cannot be created
-     */
-    private void OutputImage(BufferedImage image, String thePath) {
-        File f2 = null;
         try {
-            f2 = new File(thePath) ;
-            ImageIO.write(image,"jpg", f2);
+            f2 = new File(path2) ;
+            ImageIO.write(copy,"jpg", f2);
          } catch (IOException e) {
              System.out.println("Could not write to output image");
              System.exit(0);
          }
-    }
-}
+
+         System.out.println("Parallel Median Filtering complete!");
+
+    }//end MAIN
+
+    public class RunMedianFilterParallel extends RecursiveAction{
+        private int SEQUENTIAL_CUTOFF = 500;
+        private int starting_H;
+        private int ending_H;
+        private int windHalf = window/2;
+
+        public RunMedianFilterParallel( int start_height, int end_height) {
+            //window = window_width;
+            starting_H = start_height;
+            ending_H = end_height;
+        }
+
+        protected void computeMedianFilter(){  
+            //long startTime = System.currentTimeMillis(); //start time for benchmarking
+            int width_squared = window*window;
+            for (int i = starting_H; i < ending_H-window; i++) {
+                for (int j = 0; j < width-window; j++) {
+                    int hStart = i; int hEnd = hStart +window-1; int hMiddle = (hStart+hEnd)/2;
+                    int wStart = j; int wEnd = wStart +window-1; int wMiddle = (wStart+wEnd)/2;
+                    int rSum=0, gSum=0, bSum=0;
+                    //Going through each window
+                    for (int y = 0; y < window; y++) {
+                        
+                        for (int x = 0; x < window; x++) {
+                            int p = img.getRGB(wStart, hStart);
+                            rSum += (p>>16) & 0xff;
+                            gSum += (p>>8) & 0xff;
+                            bSum += p & 0xff;
+                            wStart++;
+                        }
+                        wStart = j;
+                        hStart++;
+                    }
+                    hStart = i;
+                    int rMean = rSum/width_squared;
+                    int gMean = gSum/width_squared;
+                    int bMean = bSum/width_squared;
+                    int filtered_pixel =  (rMean<<16) | (gMean<<8) | bMean;
+                    copy.setRGB(wMiddle, hMiddle, filtered_pixel);
+                }
+            }
+        }// **End of Mean Filter
+
+        protected void compute() {
+            if (ending_H-starting_H < SEQUENTIAL_CUTOFF) {
+                computeMedianFilter();
+                return;
+            }
+            int middle = (starting_H+ending_H)/2;
+            invokeAll(new RunMedianFilterParallel(starting_H, middle+windHalf ), new RunMedianFilterParallel( middle-windHalf, ending_H) );
+        }
+
+    }//END INNER CLASS
+}//END OUTER CLASS
